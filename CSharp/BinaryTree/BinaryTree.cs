8fef1cbb668243096faa4bc6daf22fff7291997d
Vitalii Isaenko <lookyisaenko@gmail.com>
2017-08-28 00:53:24 +0200

    Finished BST implementation

--- CSharp/BinaryTree/BinaryTree.cs
+++ CSharp/BinaryTree/BinaryTree.cs
@@ -1,11 +1,13 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Reflection.Emit;
 using System.Reflection.Metadata.Ecma335;
 using System.Text;
 
 namespace BinaryTree
 {
-    public class BinaryTree<T> where T : IComparable<T>
+    public class BinaryTree<T> : IEnumerable<T> where T : IComparable<T>
     {
         private Node<T> head;
         private int count;
@@ -95,7 +97,7 @@ namespace BinaryTree
                 return false;
             }
             count--;
-            
+
             //Case 1: Current (deleted) has no right child, so current's left child (node) replaces current
             if (nodeToRemove.RightNode == null)
             {
@@ -115,7 +117,7 @@ namespace BinaryTree
                     }
                 }
             }
-            
+
             //Case 2: Current's right child has no left, so current's right replaces current
             else if (nodeToRemove.RightNode.LeftNode == null)
             {
@@ -145,6 +147,7 @@ namespace BinaryTree
                 //Find the right's leftmost child and a parent of the child
                 var leftmostChild = nodeToRemove.RightNode.LeftNode;
                 var leftmostChildParent = nodeToRemove.RightNode;
+
                 while (leftmostChild.LeftNode != null)
                 {
                     leftmostChildParent = leftmostChild;
@@ -154,9 +157,14 @@ namespace BinaryTree
                 //The parent's left subtree becomes the leftmost's right subtree
                 leftmostChildParent.LeftNode = leftmostChild.RightNode;
 
-
                 leftmostChild.RightNode = nodeToRemove.RightNode;
                 leftmostChild.LeftNode = nodeToRemove.LeftNode;
+
+                if (parent == null)
+                {
+                    head = leftmostChild;
+                }
+
                 if (parent.CompareTo(nodeToRemove) <= 0)
                 {
                     parent.RightNode = leftmostChild;
@@ -168,7 +176,125 @@ namespace BinaryTree
             }
 
             return true;
-
         }
+
+        #region recursive silly implementations of traversal
+
+        public void PreOrderTraversal(Action<T> action)
+        {
+            PreOrderTraversal(action, head);
+        }
+
+        private void PreOrderTraversal(Action<T> action, Node<T> node)
+        {
+            if (node != null)
+            {
+                action(node.Value);
+                PreOrderTraversal(action, node.LeftNode);
+                PreOrderTraversal(action, node.RightNode);
+            }
+        }
+
+        public void PostOrderTraversal(Action<T> action)
+        {
+            PostOrderTraversal(action, head);
+        }
+
+        private void PostOrderTraversal(Action<T> action, Node<T> node)
+        {
+            if (node != null)
+            {
+                PostOrderTraversal(action, node.LeftNode);
+                PostOrderTraversal(action, node.RightNode);
+                action(node.Value);
+            }
+        }
+
+        public void InOrderTraversal(Action<T> action)
+        {
+            InOrderTraversal(action, head);
+        }
+
+        private void InOrderTraversal(Action<T> action, Node<T> node)
+        {
+            if (node != null)
+            {
+                InOrderTraversal(action, node.LeftNode);
+                action(node.Value);
+                InOrderTraversal(action, node.RightNode);
+            }
+        }
+
+        #endregion
+
+        public IEnumerator<T> InOrderTraversal()
+        {
+            if (head != null)
+            {
+                //Store the nodes we've skipped in the stack (to avoid recursion)
+                var nodes = new Stack<Node<T>>();
+
+                Node<T> currentNode = head;
+
+                //When removing recursion we need to keep track of whether ot not
+                //we should be going to the left ot the right nodes next
+                bool goLeftNext = true;
+
+                nodes.Push(head);
+
+                while (nodes.Count > 0)
+                {
+                    //If we are heading left
+                    if (goLeftNext)
+                    {
+                        //push everithing but the leftmost node to the stack
+                        //we'll yield the leftmost after this bock
+                        while (currentNode.LeftNode != null)
+                        {
+                            nodes.Push(currentNode);
+                            currentNode = currentNode.LeftNode;
+                        }
+
+                        yield return currentNode.Value;
+
+                        //if we can go right then do so
+                        if (currentNode.RightNode != null)
+                        {
+                            currentNode = currentNode.RightNode;
+
+                            //once we've gone right once, we need to start
+                            //going left again
+                            goLeftNext = true;
+                        }
+                        else
+                        {
+                            // if we can't go right then we need to pop off the parent node
+                            // so we can process it and then go to it's right child
+                            currentNode = nodes.Pop();
+                            goLeftNext = false;
+                        }
+                    }
+                }
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return InOrderTraversal();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Clear()
+        {
+            head = null;
+            count = 0;
+        }
+
+        public int Count => count;
+
     }
 }
