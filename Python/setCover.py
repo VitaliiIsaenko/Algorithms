544756e3d5eed6321ce2073a4a0a55640fbb9d8d
Vitalii Isaenko <lookyisaenko@gmail.com>
2017-08-31 11:08:59 +0200

    Finished set cover problem algorithm inplementation

--- Python/setCover.py
+++ Python/setCover.py
@@ -10,13 +10,23 @@ class RadiostationsCreator:
         stations['kfive'] = set(['ca', 'az'])
         return stations
 
-class OptimalRadiotrationsGetter():
+class OptimalRadiostationsGetter():
     states_needed = set(['mt', 'wa', 'or', 'id', 'nv', 'ut', 'ca', 'az'])
     final_stations = set()
 
-    best_station = None
-    states_coveres = set()
-    stations = RadiostationsCreator.get_stations()
-    for station, states_for_station in stations.items():
-        print(station)
-        print(states_for_station)
+    #Пока еще остались не покрытые станции
+    while states_needed:
+        best_station = None
+        states_covered_by_current_best = set()
+        stations = RadiostationsCreator.get_stations()
+        #In the loop we try to understand what station is the best for the moment
+        for station, states_for_station in stations.items():
+            #covered contains intersection of states that we STILL need to cover
+            covered = states_needed & states_for_station
+            #Here we check if current station better than previously checked (if it covers more than previous)
+            if len(covered) > len(states_covered_by_current_best):
+                best_station = station
+                states_covered_by_current_best = covered
+            final_stations.add(best_station)
+            states_needed -= states_covered_by_current_best
+    print(final_stations)
