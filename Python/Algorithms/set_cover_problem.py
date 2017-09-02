b372667fdb2e27821455e1546a000a445701f1da
Vitalii Isaenko <lookyisaenko@gmail.com>
2017-09-02 18:43:30 +0200

    rename file, refactor some code

--- Python/Algorithms/set_cover_problem.py
+++ Python/Algorithms/set_cover_problem.py
@@ -1,4 +1,5 @@
-class RadiostationsCreator:
+"""Shows an example of implementation of set cover problem algorithm"""
+class RadiostationsCreator():
     """The class generates dummy data to work with"""
     def get_stations():
         """Generates stations and states they cover"""
@@ -11,10 +12,11 @@ class RadiostationsCreator:
         return stations
 
 class OptimalRadiostationsGetter():
+    """Represents algorithm implementation"""
     states_needed = set(['mt', 'wa', 'or', 'id', 'nv', 'ut', 'ca', 'az'])
     final_stations = set()
 
-    #Пока еще остались не покрытые станции
+    #While we still have uncovered stations
     while states_needed:
         best_station = None
         states_covered_by_current_best = set()
@@ -23,7 +25,8 @@ class OptimalRadiostationsGetter():
         for station, states_for_station in stations.items():
             #covered contains intersection of states that we STILL need to cover
             covered = states_needed & states_for_station
-            #Here we check if current station better than previously checked (if it covers more than previous)
+            #Here we check if current station better than previously checked 
+            # (if it covers more than previous)
             if len(covered) > len(states_covered_by_current_best):
                 best_station = station
                 states_covered_by_current_best = covered
