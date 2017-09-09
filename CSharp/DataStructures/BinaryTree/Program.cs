8a9a69accf40ca0ce5f9ec7a216ee6448f70473f
Vitalii Isaenko <lookyisaenko@gmail.com>
2017-09-10 01:38:03 +0200

    Add Test project for BinaryTree

--- CSharp/DataStructures/BinaryTree/Program.cs
+++ CSharp/DataStructures/BinaryTree/Program.cs
@@ -8,19 +8,7 @@ namespace BinaryTree
     {
         static void Main(string[] args)
         {
-            BinarySearchTree<int> bst = new BinarySearchTree<int>()
-            {
-                1,2,3,6,4,2,76,34,0,76,45,33,2222,5,42,57,653,2345,8998,776
-            };
-            bool f = bst.Remove(5);
-            bool s = bst.Remove(8998);
 
-            foreach (var i in bst)
-            {
-                Console.WriteLine(i);
-            }
-            Console.WriteLine(f.ToString() + s);
-            Console.ReadKey();
         }
     }
 }
---
+++ CSharp/DataStructures/BinaryTreeTest/BinarySearchTreeTest.cs
@@ -0,0 +1,16 @@
+using BinaryTree;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BinaryTreeTest
+{
+    [TestClass]
+    public class BinarySearchTreeTest
+    {
+        [TestMethod]
+        public void AddTest()
+        {
+            IBinarySearchTree<string> binarySearchTree = new BinarySearchTree<string>();
+
+        }
+    }
+}
